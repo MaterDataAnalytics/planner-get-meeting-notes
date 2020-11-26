f6405159cce8025e0327554d917aa73720d88661
Motygullina <Alina.Motygullina@mater.org.au>
2020-11-26 15:36:31 +1000

    clean Body content removed completely

--- parameters.py
+++ parameters.py
@@ -1,18 +1,19 @@
-
 # user input
-meetingDate = '2020-11-6'
+meetingDate = '2020-11-4'
 planId = '-6-Vic94GE-s5oiwg57nAsgAFOm6'
 maxDays = 30
 
-# Clean BodyContent from TaskPost table
-params = (meetingDate, planId, maxDays)
-taskPostColumnNames=['PlanId', 'TaskId', 'PostId', 'CreatedDateTime', 'LoadDate', 'BodyContent', 'CleanBodyContent']
-
 # make meeting notes
-query_sp = 'exec [MDA-DB-DEV-CLA-AE].planner.createMeetingNotes2 ?, ?, ?'
-meeting_notes_columns = ['Section', 'Card Created DateTime', 'Bucket', 'Card Title', 'Card Labels', 'Date Description', 'Task Description', 'Comments', 'Assigneess']
+meeting_notes_columns = ['Section', 'Card Created DateTime', 'Bucket', 'Card Title', 'Card Labels', 'Date Description',
+                         'Task Description', 'Comments', 'Assignees']
 meeting_notes_index_col = 'Section'
 
-# send email
+# send email server
 smtp_server = 'smtp.mater.org.au'
 smtp_port = '587'
+
+# email params
+send_from = 'alina.motygullina@mater.org.au'
+send_to = 'alina.motygullina@mater.org.au'
+subject = f'Meeting notes {meetingDate}'
+body = 'Please find the file attached'
--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import pyodbc
 import pandas as pd
 import io
 import smtplib
@@ -7,8 +6,7 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from database import query_to_df, exec_procedure_to_df
-from database import clearHtml
+from database import exec_procedure_to_df
 
 from config import cs
 # cs should be of the following format:
@@ -23,44 +21,7 @@ from config import cs
 #     )
 from config import my_outlook_username, my_outlook_password
 
-from parameters import query_sp, meetingDate, planId, maxDays,meeting_notes_columns, meeting_notes_index_col
-
-def clean_body_content(cs, query_tp, cols):
-
-    # get the dataframe
-    df = query_to_df(cs, query_tp, cols=cols)
-
-    # clean BodyContent
-    df['CleanBodyContent'] = df['BodyContent'].apply(lambda x: clearHtml(x))
-
-    # create a pyodbc connection
-    conn = pyodbc.connect(cs)
-
-    cursor = conn.cursor()
-    conn.autocommit = True
-
-    # loop throgh the rows to update value
-    for i, row in df.iterrows():
-        query_insert_tp = """
-        update [planner].[TaskPost]
-        set CleanBodyContent = ?
-        where PlanId = ?
-          and TaskId = ?
-          and PostId = ?
-          and CreatedDateTime = ?
-          and LoadDate = ?
-          and CleanBodyContent is NULL
-        """
-        cursor.execute(query_insert_tp, (
-            row[cols[6]]
-            , row[cols[0]]
-            , row[cols[1]]
-            , row[cols[2]]
-            , row[cols[3]]
-            , row[cols[4]]
-        )
-                       )
-    conn.close()
+from parameters import meetingDate, planId, maxDays, meeting_notes_columns, meeting_notes_index_col, send_from, send_to, subject, body
 
 def replace_breaks(df):
     '''
@@ -69,12 +30,14 @@ def replace_breaks(df):
     df = df.replace('<br/>', '\n', regex=True)
     return df
 
+
 def export_excel(df):
     with io.BytesIO() as buffer:
-        writer=pd.ExcelWriter(buffer)
+        writer = pd.ExcelWriter(buffer)
         df.to_excel(writer)
         writer.save()
         return buffer.getvalue()
+
 
 def send_dataframe(username, password, send_from, send_to, subject, body, df):
     multipart = MIMEMultipart()
@@ -90,7 +53,7 @@ def send_dataframe(username, password, send_from, send_to, subject, body, df):
         multipart.attach(attachment)
     multipart.attach(MIMEText(body, 'html'))
     try:
-        server = smtplib.SMTP('smtp.mater.org.au','587')
+        server = smtplib.SMTP('smtp.mater.org.au', '587')
     except smtplib.socket.gaierror:
         return False
     server.ehlo()
@@ -107,20 +70,22 @@ def send_dataframe(username, password, send_from, send_to, subject, body, df):
     print('Email sent successfully')
     server.quit()
 
+
 def run():
+
+    query_sp = 'exec [MDA-DB-DEV-CLA-AE].planner.createMeetingNotes2 ?, ?, ?'
+
     # exec proc
     df = exec_procedure_to_df(cs=cs, query=query_sp, meetingDate=meetingDate, planId=planId, maxDays=maxDays,
                               meeting_notes_columns=meeting_notes_columns,
                               meeting_notes_index_col=meeting_notes_index_col)
 
     # Clean the line breaks
-    df = df.replace('<br/>', '\n', regex=True)
 
     # send the dataframe
-    send_dataframe(username=my_outlook_username, password=my_outlook_password, send_from='alina.motygullina2@mater.org.au', send_to='alina.motygullina2@mater.org.au',
-                   subject='TEST: sending meeting notes with Python',
-                   body='This is test. \nPlease find the file attached', df=df)
+    send_dataframe(username=my_outlook_username, password=my_outlook_password, send_from=send_from, send_to=send_to,
+                   subject=subject, body=body, df=df)
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     run()
