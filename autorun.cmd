904e0ff5060e6a4274f00ba30bece02997fd753b
Alina Motygullina <alina.motygullina@gmail.com>
2021-02-17 11:53:37 +1000

    added coments to autorun.cmd why it does not work now

--- autorun.cmd
+++ autorun.cmd
@@ -2,5 +2,5 @@
 :: This was not working, something is wrong with the spaces
 :: python "%~dp0run.py" >> "d:\Logs\PlannerMeetingNotes-%date:~10,4%%date:~7,2%%date:~4,2%.log" 2>>&1
 
-:: This works
+:: This works but complains about not being able to find a python. The path need to be added
 python "%~dp0run.py" >> "d:\Logs\PlannerMeetingNotes.log" 2>>&1
